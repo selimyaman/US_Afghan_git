f02651be1627f74eb74fe3ac51e74646979b7401
Selim Yaman <selimyaman26@gmail.com>
2022-03-21 15:01:24 -0400

    more readable readme

--- 01- Tweet Pull/tweet_pull (full sample).sh
+++ 01- Tweet Pull/tweet_pull (full sample).sh
@@ -1,13 +1,9 @@
-# SOURCE: https://twarc-project.readthedocs.io/en/latest/twarc2_en_us/
-
-# TWARC2 METHOD.
-
-#configure deyip bearer token'i girmek lazim.
+#enter bearer token here
 twarc2 configure
 
-# bu da search icin
+# then search
 twarc2 search --archive --limit 100 --no-context-annotations --start-time 2021-05-01 --end-time 2021-11-18 '(taliban OR taleban OR afghanistan OR afghan OR (Islamic Emirate) OR kabul OR pashtun OR pashto OR kandahar OR pashtuns OR (Ashraf Ghani)) lang:en -is:retweet' twarc2_tweets_half.json
 #şurda bitti: Processed 2 months/6 months [17:39:49<22:36:17, 9000252 tweets total]. Yaklaşık 15 saat aldı.
 
-#json dosyasini flatten edip csv'e cevirmek icin:
+#flatten json file by converting it to csv
 twarc2 csv twarc2_tweets_half.json tweets_half.csv
